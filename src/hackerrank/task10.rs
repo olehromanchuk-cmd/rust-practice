ab8614540344bd9b2d2e740b7a3694edb6b0a5a2
Oleh <oleh.romanchuk@e-u.edu.ua>
2026-04-24 02:16:42 +0300

    Practice 10 sock merchant

---
+++ src/hackerrank/task10.rs
@@ -0,0 +1,20 @@
+pub fn sock_merchant(ar: &[i32]) -> i32 {
+    let mut counts = [0; 101];
+
+    for &color in ar {
+        counts[color as usize] += 1;
+    }
+
+    counts.iter().map(|count| count / 2).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sock_merchant() {
+        let ar = vec![10, 20, 20, 10, 10, 30, 50, 10, 20];
+        assert_eq!(sock_merchant(&ar), 3);
+    }
+}
