7e06ff545d3bbe13a712620472d847782b7ce1a2
Oleh <oleh.romanchuk@e-u.edu.ua>
2026-05-12 09:15:23 +0300

    Add solution for practice 15 drawing book

---
+++ src/hackerrank/task15.rs
@@ -0,0 +1,21 @@
+pub fn page_count(n: i32, p: i32) -> i32 {
+    let from_front = p / 2;
+    let from_back = n / 2 - p / 2;
+
+    from_front.min(from_back)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_page_count_from_front() {
+        assert_eq!(page_count(6, 2), 1);
+    }
+
+    #[test]
+    fn test_page_count_from_back() {
+        assert_eq!(page_count(5, 4), 0);
+    }
+}
